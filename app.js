d0f8c9d4004413f085fb3683743f910a7cad5456
Jordan Talbot <jordanelitalbot@gmail.com>
2019-05-14 20:07:53 -0700

    Added the ability to add income and expenses

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
 // Budget Controller
 var budgetController = (function() {
-    
+    var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
+
+	return {
+		addItem: function(type, desc, val) {
+			var newItem, ID;
+
+			// Create new ID
+			if (data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+			} else {
+				ID = 0;
+			}
+
+			// Create new item based on type
+			if (type === 'exp') {
+				newItem = new Expense(ID, desc, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, desc, val);
+			}
+			// Pushing newItem into the correct array
+			data.allItems[type].push(newItem);
+			return newItem;
+		},
+
+		testing: function() {
+			console.log(data);
+		}
+	};
 
 })();
+
 
 // UI Controller
 var UIController = (function() {
@@ -19,8 +68,8 @@ var UIController = (function() {
         getInput: function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
-                 description: document.querySelector(DOMstrings.inputDescription).value, 
-                 value: document.querySelector(DOMstrings.inputValue).value
+                description: document.querySelector(DOMstrings.inputDescription).value, 
+                value: document.querySelector(DOMstrings.inputValue).value
             };
         },
 
@@ -31,30 +80,45 @@ var UIController = (function() {
 
 })();
 
+
 // Global App Controller
 var controller = (function(budgetCtrlr, UICtrlr) {
+    
+    var setupEventListeners = function() {
+		var DOM = UICtrlr.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(e) {
+            if (e.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
     
     var DOM = UICtrlr.getDOMstrings();
 
     var ctrlAddItem = function() {
+		var input, newItem;
         // 1. Get the filed input data
-        var input = UICtrlr.getInput();
-        console.log(input);
-
+        input = UICtrlr.getInput();
 
         // 2. Add the item to the budget controller
-
-        // 3. Add the item to the UI
+		newItem = budgetCtrlr.addItem(input.type, input.description, input.value);
+		
+		// 3. Add the item to the UI
 
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-    }
+	};
+	
+	return {
+		init: function() {
+			setupEventListeners();
+			console.log('App has started');
+		}
+	};
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-    document.addEventListener('keypress', function(e) {
-        if (e.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
 })(budgetController, UIController);
+
+controller.init();
