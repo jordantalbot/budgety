4f9bd1080d25cd8bb1ab2e1e6f7189df55f35bc4
Jordan Talbot <jordanelitalbot@gmail.com>
2019-05-17 19:53:37 -0700

    Added budget display functionality.

--- app.js
+++ app.js
@@ -99,7 +99,11 @@ var UIController = (function() {
         inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
     }
     
     return {
@@ -144,14 +148,24 @@ var UIController = (function() {
 			}); 
 			
 			fieldsArr[0].focus();
+		},
 
+		displayBudget: function(obj) {
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '--%';
+			}
 		},
 
         getDOMstrings: function() {
             return DOMstrings;
         }
     };
-
 })();
 
 
@@ -178,7 +192,7 @@ var controller = (function(budgetCtrlr, UICtrlr) {
 		var budget = budgetCtrlr.getBudget();
 
 		// 3. Display the budget on the UI
-		console.log(budget);	
+		UICtrlr.displayBudget(budget);
 
 	};
 
@@ -206,9 +220,14 @@ var controller = (function(budgetCtrlr, UICtrlr) {
 		init: function() {
 			setupEventListeners();
 			console.log('App has started');
+			UICtrlr.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 		}
 	};
-
 })(budgetController, UIController);
 
 controller.init();
